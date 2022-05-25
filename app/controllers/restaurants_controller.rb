442c975947279151fea8733bc52da616b9204909
Marlon Wanderllich <marlonwlv@gmail.com>
2022-05-25 11:04:03 -0300

    Finish User Stories

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,40 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    # validate the values
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
